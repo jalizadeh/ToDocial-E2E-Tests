d81ab0748f789e86103099cd7858b19d7cd7b073
jalizadeh <jalizade3d@yahoo.com>
2024-03-07 19:23:06 +0100

    Preparing for Jenkins

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -5,6 +5,7 @@ import io.cucumber.testng.CucumberOptions;
 import main.java.common.CucumberTestNGRunner;
 
 @CucumberOptions(
+        plugin={"pretty", "html:target/cucumber", "json:target/cucumber.json"},
         features = "src/test/java/features",
         glue={"test.java.steps"}
 )
